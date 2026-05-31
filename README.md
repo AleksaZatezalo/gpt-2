60dc914d12dc64a85548ff22175df634603e0fd9
Aleksa <zabumaphu@gmail.com>
2026-05-31 13:16:41 -0400

    Files Created

---
+++ README.md
@@ -0,0 +1,3 @@
+# GPT 2
+
+Implementing GPT-2 alongisde a Andrej Karpathy tutorial.
---
+++ micrograd.py
@@ -0,0 +1,5 @@
+"""
+Description: Implementing Micrograd by Andrej Karpathy
+Author: Aleksa Zatezalo
+Date: May 2026
+"""
